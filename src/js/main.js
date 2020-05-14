a629460d2abfd04e643103ab62bd0ac387adc2a4
Gevork Gevorkyan <bmstu.gg@gmail.com>
2020-05-14 19:16:51 +0300

    ADD mobile-menu button click event

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,13 @@
 //=require ../common.blocks/nav.js
 //=require ../common.blocks/offers-menu.js
 //=require ../common.blocks/restaurant-menu.js
+
+const headerMenuToggle = document.querySelector('#nav__toggle');
+const headerMenu = document.querySelector('.nav__list');
+
+const onHeaderMenuToggleClick = (evt) => {
+  evt.target.classList.toggle('nav__toggle--toggled');
+  headerMenu.classList.toggle('nav__list--toggled');
+};
+
+headerMenuToggle.addEventListener('click', onHeaderMenuToggleClick);
