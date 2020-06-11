28a66d5f1179c14de73e0522b64498d1019822f7
Gevork Gevorkyan <bmstu.gg@gmail.com>
2020-06-12 01:55:08 +0300

    Update main.ts

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,1 +1,6 @@
+import {monthSpecialsMock} from "./mock/month-specials";
+import MonthSpecialsController from "./controllers/month-specials-controller";
 
+const monthSpecialContainer = document.querySelector(`.month-specials .container`);
+const monthSpecialsController = new MonthSpecialsController(monthSpecialContainer!, monthSpecialsMock);
+monthSpecialsController.init();
