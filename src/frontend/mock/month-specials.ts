1a676270f16b1460b26f7423378279d1e7871f0a
Gevork Gevorkyan <bmstu.gg@gmail.com>
2020-06-12 01:55:04 +0300

    Update month-specials.ts

--- src/frontend/mock/month-specials.ts
+++ src/frontend/mock/month-specials.ts
@@ -1,43 +1,38 @@
-import {getRandomNum} from "../utils";
+import {getRandomElement, getRandomNum} from "../utils";
+import {MonthSpecialType} from "../types";
 
-
-type Currency = '$' | '₽';
-
-interface IMonthSpecial {
-  title: string;
-  description: string;
-  price: {
-    value: number;
-    currency: Currency;
-  }
-}
-
-const getMonthSpecial = (): IMonthSpecial => ({
-  title: [`Greens fava`, `Greens fava`, `Pea horseradish`, `Soko radicchio`, `Tigernut`, `Yarrow sweet`][getRandomNum(0, 5)],
-  description: [`Lorem ipsum dolor sit Pellentesque vel enim a`, `Lorem ipsum dolor sit`, `Pellentesque vel enim a`][getRandomNum(0, 2)],
-  price: {
-    value: getRandomNum(5, 100),
-    currency: ["$", "₽"][getRandomNum(0, 1)] as Currency
-  }
+const titles: string[] = [
+  `Greens fava`,
+  `Pea horseradish`,
+  `Soko radicchio`,
+  `Tigernut`,
+  `Yarrow sweet`];
+const descriptions: string[] = [
+  `Lorem ipsum dolor sit Pellentesque vel enim a`,
+  `Lorem ipsum dolor sit`,
+  `Pellentesque vel enim a`];
+const imgs = [
+  `month-specials-offers-1.jpg`,
+  `month-specials-offers-2.jpg`,
+  `month-specials-offers-3.jpg`,
+  `month-specials-offers-4.jpg`,
+  `month-specials-offers-5.jpg`,
+  `month-specials-offers-6.jpg`];
+const price: {min: number, max: number} = {
+  min: 5,
+  max: 100
+};
+const getMonthSpecial = (): MonthSpecialType => ({
+  title: getRandomElement<string>(titles),
+  description: getRandomElement<string>(descriptions),
+  img: getRandomElement<string>(imgs),
+  price: getRandomNum(price.min, price.max)
 });
 
-interface IMonthSpecialsConfig {
-  amount: {
-    min: number;
-    max: number
-  }
-}
 
-const monthSpecialsConfig: IMonthSpecialsConfig = {
-  amount: {
-    min: 4,
-    max: 8
-  }
-};
-
-const monthSpecials: IMonthSpecial[] = new Array(getRandomNum(monthSpecialsConfig.amount.min, monthSpecialsConfig.amount.max))
+const monthSpecialsAmount: number = 6;
+const monthSpecialsMock: MonthSpecialType[] = new Array(monthSpecialsAmount)
   .fill(``)
   .map(() => getMonthSpecial());
 
-
-export {monthSpecials};
+export {monthSpecialsMock};
