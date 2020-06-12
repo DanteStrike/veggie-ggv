a194f5814aa27b7a9d03f361f9f609b465f58c27
Gevork Gevorkyan <bmstu.gg@gmail.com>
2020-06-13 00:16:55 +0300

    REDESIGN month specials mock

--- src/frontend/components/month-specials-view/month-specials-view.test.ts
+++ src/frontend/components/month-specials-view/month-specials-view.test.ts
@@ -1,17 +1,7 @@
 import MonthSpecialsView from "./month-specials-view";
+import {monthSpecialsMock} from "../../mock/month-specials";
 
 it(`Month specials component should render correctly`, () => {
-  const monthSpecials = [{
-    title: `T1`,
-    description: `D1`,
-    img: `I1`,
-    price: 1
-  }, {
-    title: `T2`,
-    description: `D2`,
-    img: `I2`,
-    price: 2
-  }];
-  const monthSpecialsView = new MonthSpecialsView(monthSpecials);
+  const monthSpecialsView = new MonthSpecialsView(monthSpecialsMock);
   expect(monthSpecialsView.getElement()).toMatchSnapshot();
 });
--- src/frontend/mock/month-specials.ts
+++ src/frontend/mock/month-specials.ts
@@ -1,38 +1,33 @@
-import {getRandomElement, getRandomNum} from "../utils";
 import {MonthSpecialType} from "../types";
 
-const titles: string[] = [
-  `Greens fava`,
-  `Pea horseradish`,
-  `Soko radicchio`,
-  `Tigernut`,
-  `Yarrow sweet`];
-const descriptions: string[] = [
-  `Lorem ipsum dolor sit Pellentesque vel enim a`,
-  `Lorem ipsum dolor sit`,
-  `Pellentesque vel enim a`];
-const imgs = [
-  `month-specials-offers-1.jpg`,
-  `month-specials-offers-2.jpg`,
-  `month-specials-offers-3.jpg`,
-  `month-specials-offers-4.jpg`,
-  `month-specials-offers-5.jpg`,
-  `month-specials-offers-6.jpg`];
-const price: {min: number, max: number} = {
-  min: 5,
-  max: 100
-};
-const getMonthSpecial = (): MonthSpecialType => ({
-  title: getRandomElement<string>(titles),
-  description: getRandomElement<string>(descriptions),
-  img: getRandomElement<string>(imgs),
-  price: getRandomNum(price.min, price.max)
-});
-
-
-const monthSpecialsAmount: number = 6;
-const monthSpecialsMock: MonthSpecialType[] = new Array(monthSpecialsAmount)
-  .fill(``)
-  .map(() => getMonthSpecial());
-
-export {monthSpecialsMock};
+export const monthSpecialsMock: MonthSpecialType[] = [{
+    title: `Greens fava`,
+    description: `Lorem ipsum`,
+    img: `month-specials-offers-1.jpg`,
+    price: 10
+  }, {
+    title: `Celery quandong`,
+    description: `Lorem ipsum dolor sit`,
+    img: `month-specials-offers-2.jpg`,
+    price: 20
+  }, {
+    title: `Pea horseradish`,
+    description: `Lorem ipsum dolor sit Pellentesque`,
+    img: `month-specials-offers-3.jpg`,
+    price: 30
+  }, {
+    title: `Soko radicchio`,
+    description: `Lorem ipsum dolor sit Pellentesque vel enim a`,
+    img: `month-specials-offers-4.jpg`,
+    price: 40
+  }, {
+    title: `Tigernut`,
+    description: `Pellentesque vel enim a`,
+    img: `month-specials-offers-5.jpg`,
+    price: 50
+  }, {
+    title: `Yarrow sweet`,
+    description: `Lorem vel enim a`,
+    img: `month-specials-offers-6.jpg`,
+    price: 60
+  }];
